e0c664692030a9c8709616b054dbce72cc220534
“Michael <tur87428@temple.edu>
2025-09-05 18:38:21 -0400

    lastdraft

--- random.c
+++ random.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-const char* rand_string(){
-    return "Hello darknis";
+char randy[10000];
+
+const char* rand_string(int length){
+    
+    for(int i = 0; i < length; i++){
+        randy[i] = (32 + rand() % (126 - 32 + 1));
+    }
+    
+    return randy;
 }
 
 int main(int argc, char **argv){
-    printf("%s\n", rand_string());
+    srand(time(NULL));
+    int length = 0;
+    
+    printf("Length of string: ");
+    scanf("%d", &length);
+    
+    printf("%s\n", rand_string(length));
     return 0;
 }
